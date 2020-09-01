53320653276d0b9051954219c369d83427d99a4e
jackychancjcjcj <chenjian2018biu@gmail.com>
2020-09-01 15:19:51 +0800

    提交django网站

---
+++ 网站/users/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.contrib.auth import logout, login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+
+# Create your views here.
+
+def logout_view(requesst):
+    logout(requesst)
+    return HttpResponseRedirect(reverse('learning_logs:index'))
+
+def register(request):
+    if request.method != 'POST':
+        form = UserCreationForm()
+    else:
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            authenticated_user = authenticate(username=new_user,password=request.POST['password1'])
+            login(request,authenticated_user)
+            return HttpResponseRedirect(reverse('learning_logs:index'))
+    context = {'form':form}
+    return render(request, 'users/register.html', context)
